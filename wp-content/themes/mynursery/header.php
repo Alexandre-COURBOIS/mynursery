beba3c985e0d723971c587546dc1e8b412a736fe
stephane <github@lecuyer.eu>
2020-03-19 18:21:44 +0100

    Working on the footer of the pages:   - Inclusion of bootstrap
    To do : See to put the logos of the social networks and see for the font

---
+++ wp-content/themes/mynursery/header.php
@@ -0,0 +1,16 @@
+<?php global $web ?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Nursery</title>
+    <meta name="description" content="">
+    <link href="https://fonts.googleapis.com/css?family=Arvo:400,700" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
