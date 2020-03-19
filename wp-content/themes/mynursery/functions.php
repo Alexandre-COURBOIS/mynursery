f5fa6905350088b63086daba61ab07665d14e72d
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2020-03-19 12:45:14 +0100

    adding gitignore for wordpress

---
+++ wp-content/themes/mynursery/functions.php
@@ -0,0 +1,5 @@
+<?php
+
+require get_template_directory() . '/inc/generale.php';
+
+require get_template_directory() . '/inc/parameters.php';
---
+++ wp-content/themes/mynursery/inc/parameters.php
@@ -0,0 +1,12 @@
+<?php
+
+global $web;
+
+/*$web = array(
+    'pages' => array(
+        'home' => array(
+            'id' => 5,
+            'slug' => '/'
+        )
+    )
+);*/
