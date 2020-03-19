b0d3e86be8cbfa60133baf132b0654817e88fdf1
Yann Poher <yan.poher@gmail.com>
2020-03-19 18:18:02 +0100

    about-us section html and bootstrap : work still in progress

--- wp-content/themes/mynursery/template-home.php
+++ wp-content/themes/mynursery/template-home.php
@@ -2,3 +2,40 @@
 /*
 Template Name: Home
 */
+get_header();
+?>
+    <body>
+
+    <div id="wrap-about-us" class=".container">
+        <section id="about-us" class="row">
+            <div id="about-us-left" class="col-sm">
+                <h2 class="sections">about us</h2>
+                <h3 class="titles">Ici le titre de la section about us</h3>
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. A non odio pariatur, ullam veniam
+                    voluptatum!
+                    Assumenda, aut autem delectus doloremque, dolores error est illum perferendis porro totam vitae
+                    voluptas voluptatem.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aspernatur cumque cupiditate
+                    debitis harum laudantium magni minima modi, quia rerum, saepe sapiente suscipit!
+                    Adipisci atque fuga nulla qui sit tempore.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A non odio pariatur, ullam veniam
+                    voluptatum!
+                    Assumenda, aut autem delectus doloremque, dolores error est illum perferendis porro totam vitae
+                    voluptas voluptatem.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aspernatur cumque cupiditate
+                    debitis harum laudantium magni minima modi, quia rerum, saepe sapiente suscipit!
+                    Adipisci atque fuga nulla qui sit tempore.
+                </p>
+                <a class="btn-redirection" href="http///www.ici-le-lien.fr">En savoir plus...</a>
+            </div>
+
+            <div id="about-us-right" class="col-sm">
+                <img src="<?php echo get_template_directory_uri() ?>/asset/img/about-us-family.jpg" alt="">
+            </div>
+        </section>
+    </div>
+    </body>
+
+
+<?php
+get_footer();
