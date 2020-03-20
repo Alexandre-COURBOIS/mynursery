821e8ce2952f96c4928fab521da940f9d38b8850
stephane <github@lecuyer.eu>
2020-03-20 17:09:30 +0100

    Section latest new :  - Creation of files  - Importing 2 random images  - Layout under construction

--- wp-content/themes/mynursery/template-home.php
+++ wp-content/themes/mynursery/template-home.php
@@ -34,7 +34,38 @@ get_header();
                 <img src="<?php echo get_template_directory_uri() ?>/asset/img/about-us-family.jpg" alt="">
             </div>
         </section>
+
+
+        <section class="section-latest-new">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h2 class="title-section">Latest New</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/covid19.png" alt="" class="img-new">
+                        <h3 class="title-new">Covid-19 et les crèches, que ce passe-t-il ?</h3>
+                        <span>Publié le : 18 Mars 2020</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.Ut enim ad minim binz veniam, quis nostrud exercitation
+                            ullamco laboris nisi ut...</p>
+                    </div>
+                    <div class="col-lg-6">
+                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/creche.jpg" alt="" class="img-new">
+                        <h3 class="title-new">Crèche 2.0, quelles sont les nouveautées ?</h3>
+                        <span>Publié le : 2 Mars 2020</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporincididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip...</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
+
+
     </body>
 
 
