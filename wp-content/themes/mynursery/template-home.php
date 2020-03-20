9755529aa39f2d3aebe7a5f0827d3480a57a0576
Yann Poher <yan.poher@gmail.com>
2020-03-20 17:08:22 +0100

    section about us : bootstrap use to position elements then work on css on left side button need javascript

--- wp-content/themes/mynursery/template-home.php
+++ wp-content/themes/mynursery/template-home.php
@@ -6,33 +6,25 @@ get_header();
 ?>
     <body>
 
-    <div id="wrap-about-us" class=".container">
-        <section id="about-us" class="row">
-            <div id="about-us-left" class="col-sm">
-                <h2 class="sections">about us</h2>
-                <h3 class="titles">Ici le titre de la section about us</h3>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. A non odio pariatur, ullam veniam
+    <div class="container">
+        <div id="about-us" class="row">
+            <div id="about-us-left" class="col-md-7">
+                <h2 id="h2" class="section_aboutus">about us</h2>
+                <h3 id="h3" class="title_aboutus">Ici le titre de la section about us</h3>
+                <p class="text_aboutus"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. A non odio pariatur, ullam veniam
                     voluptatum!
                     Assumenda, aut autem delectus doloremque, dolores error est illum perferendis porro totam vitae
                     voluptas voluptatem.
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aspernatur cumque cupiditate
                     debitis harum laudantium magni minima modi, quia rerum, saepe sapiente suscipit!
-                    Adipisci atque fuga nulla qui sit tempore.
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A non odio pariatur, ullam veniam
-                    voluptatum!
-                    Assumenda, aut autem delectus doloremque, dolores error est illum perferendis porro totam vitae
-                    voluptas voluptatem.
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aspernatur cumque cupiditate
-                    debitis harum laudantium magni minima modi, quia rerum, saepe sapiente suscipit!
-                    Adipisci atque fuga nulla qui sit tempore.
                 </p>
-                <a class="btn-redirection" href="http///www.ici-le-lien.fr">En savoir plus...</a>
+                <a id="test" class="btn-redirection" href="http///www.ici-le-lien.fr">En savoir plus...</a>
             </div>
 
-            <div id="about-us-right" class="col-sm">
-                <img src="<?php echo get_template_directory_uri() ?>/asset/img/about-us-family.jpg" alt="">
+            <div id="about-us-right" class="col-md-5">
+                <img class="img-thumbnail img-fluid mx-auto"  src="<?php echo get_template_directory_uri() ?>/asset/img/about-us-family2.jpg" alt="">
             </div>
-        </section>
+        </div>
     </div>
     </body>
 
