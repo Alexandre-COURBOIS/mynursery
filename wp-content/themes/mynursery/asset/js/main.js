c9af427c9e8a9ec27f7431b4172eb2df05004045
pierrehlvt <pierre.halavent@gmail.com>
2020-03-23 14:45:50 +0100

    form-style
    - remove js function that wasn't working

--- wp-content/themes/mynursery/asset/js/main.js
+++ wp-content/themes/mynursery/asset/js/main.js
@@ -16,14 +16,4 @@ function myFunction() {
     }
 }
 
-(function($) {
-
-    $(".form-style .form-control").on("input", function() {
-        if ($(this).val()) {
-            $(this).addClass("hasValue");
-        } else {
-            $(this).removeClass("hasValue");
-        }
-    });
-});
 
