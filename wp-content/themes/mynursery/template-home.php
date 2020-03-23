280547a6aac2451fe9f305c19b9ee1a6cf1c39a3
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2020-03-23 17:29:38 +0100

    hotfix on section on template-home, adjust wrap size and placement section

--- wp-content/themes/mynursery/template-home.php
+++ wp-content/themes/mynursery/template-home.php
@@ -5,10 +5,11 @@ Template Name: Home
 
 get_header();
 ?>
+
     <section>
     <div class="wrap-slider">
         <div id="demo" class="carousel slide carousel-fade" data-ride="carousel">
-              <!-- Indicators -->
+            <!-- Indicators -->
             <ul class="carousel-indicators">
                 <li data-target="#demo" data-slide-to="0" class="active"></li>
                 <li data-target="#demo" data-slide-to="1"></li>
@@ -35,9 +36,9 @@ get_header();
                     <div class="carousel-caption">
                         <h3 class="h2">Give them some good time !</h3>
                         <p>We've a lot of activities to make them grow</p>
-                  </div>
-              </div>    
-                    <div class="carousel-item">
+                    </div>
+                </div>
+                <div class="carousel-item">
                     <img class="d-block img-fluid"
                          src="<?php echo get_template_directory_uri() ?>/asset/img/slider3.jpg"
                          alt="Enfants dans un champ de coquelicots York">
@@ -55,10 +56,11 @@ get_header();
             <a class="carousel-control-next" href="#demo" data-slide="next">
                 <span class="carousel-control-next-icon"></span>
             </a>
-    </div>
+        </div>
     </div>
     <section>
 
+    <div class="separator"></div>
 
     <section>
         <div class="container">
@@ -82,125 +84,6 @@ get_header();
                     <img class="img-thumbnail img-fluid mx-auto"
                          src="<?php echo get_template_directory_uri() ?>/asset/img/about-us-family2.jpg" alt="">
                 </div>
-    </section>
-
-    <section class="partner-section">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <h2 class="title-section">Our Clients</h2>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="logo-hover-zoom img-hover-zoom-translate">
-                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/creche1.png" alt="" class="logo-client">
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="logo-hover-zoom img-hover-zoom-translate">
-                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/toupoutou.png" alt="" class="logo-client">
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="logo-hover-zoom img-hover-zoom-translate">
-                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/creche3.png" alt="" class="logo-client">
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="logo-hover-zoom img-hover-zoom-translate">
-                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/creche4.png" alt="" class="logo-client">
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="logo-hover-zoom img-hover-zoom-translate">
-                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/creche5.png" alt="" class="logo-client">
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="logo-hover-zoom img-hover-zoom-translate">
-                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/creche6.png" alt="" class="logo-client">
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="logo-hover-zoom img-hover-zoom-translate">
-                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/creche7.png" alt="" class="logo-client">
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="logo-hover-zoom img-hover-zoom-translate">
-                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/creche8.png" alt="" class="logo-client">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="section-latest-new">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <h2 class="title-section">Latest New</h2>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="img-hover-zoom img-hover-zoom-translate">
-                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/covid19.png" alt=""
-                             class="img-new">
-                    </div>
-                    <h3 class="title-new">Covid-19 et les crèches, que ce passe-t-il ?</h3>
-                    <span>Publié le : 18 Mars 2020</span>
-                    <p class="resume-new">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                        incididunt
-                        ut
-                        labore et dolore magna aliqua.Ut enim ad minim binz veniam, quis nostrud exercitation
-                        ullamco laboris nisi ut...</p>
-                </div>
-                <div class="col-lg-6">
-                    <div class="img-hover-zoom img-hover-zoom-translate">
-                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/creche.jpg" alt=""
-                             class="img-new">
-
-                    </div>
-                    <h3 class="title-new">Crèche 2.0, quelles sont les nouveautées ?</h3>
-                    <span>Publié le : 2 Mars 2020</span>
-                    <p class="resume-new">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        temporincididunt
-                        ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco
-                        laboris nisi ut aliquip...</p>
-                </div>
-
-
-    <div class="separator"></div>
-
-    <section>
-        <div class="container">
-            <div id="about-us" class="row">
-                <div id="about-us-left" class="col-md-7">
-                    <h2 id="h2" class="section_aboutus">about us</h2>
-                    <h3 id="h3" class="title_aboutus">Ici le titre de la section about us</h3>
-
-                    <p class="text_aboutus"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. A non odio
-                        pariatur, ullam veniam
-                        voluptatum!
-                        Assumenda, aut autem delectus doloremque, dolores error est illum perferendis porro totam vitae
-                        voluptas voluptatem.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aspernatur cumque cupiditate
-                        debitis harum laudantium magni minima modi, quia rerum, saepe sapiente suscipit!
-                    </p>
-
-                    <a id="supprlink" class="btn btn-outline-dark" role="button" href="http///www.ici-le-lien.fr">En
-                        savoir plus... <i class="fas fa-arrow-right"></i></a>
-                </div>
-
-
-                <div id="about-us-right" class="col-md-5">
-                    <img class="img-thumbnail img-fluid mx-auto"
-                         src="<?php echo get_template_directory_uri() ?>/asset/img/about-us-family2.jpg" alt="">
-                </div>
             </div>
         </div>
     </section>
@@ -208,7 +91,7 @@ get_header();
     <div class="separator"></div>
 
     <section class="ourspecilization">
-        <div class="container">
+        <div class="wrap-slider">
             <div class="outer-div">
                 <div class="inner-div">
                     <h2 id="h2" class="section_ourspe">our specilization</h2>
@@ -237,6 +120,7 @@ get_header();
                         commodo.</p>
                 </div>
             </div>
+        </div>
     </section>
 
     <div class="separator"></div>
@@ -277,9 +161,70 @@ get_header();
                         laboris nisi ut aliquip...</p>
                 </div>
             </div>
+        </div>
     </section>
 
     <div class="separator"></div>
+
+    <section class="partner-section">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h2 class="title-section">Our Clients</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="logo-hover-zoom img-hover-zoom-translate">
+                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/creche1.png" alt=""
+                             class="logo-client">
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="logo-hover-zoom img-hover-zoom-translate">
+                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/toupoutou.png" alt=""
+                             class="logo-client">
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="logo-hover-zoom img-hover-zoom-translate">
+                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/creche3.png" alt=""
+                             class="logo-client">
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="logo-hover-zoom img-hover-zoom-translate">
+                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/creche4.png" alt=""
+                             class="logo-client">
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="logo-hover-zoom img-hover-zoom-translate">
+                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/creche5.png" alt=""
+                             class="logo-client">
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="logo-hover-zoom img-hover-zoom-translate">
+                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/creche6.png" alt=""
+                             class="logo-client">
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="logo-hover-zoom img-hover-zoom-translate">
+                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/creche7.png" alt=""
+                             class="logo-client">
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="logo-hover-zoom img-hover-zoom-translate">
+                        <img src="<?php echo get_template_directory_uri() ?>/asset/img/creche8.png" alt=""
+                             class="logo-client">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 <?php
 get_footer();
