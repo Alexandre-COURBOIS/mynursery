82c936a905c9c222b7ecb871ecfc57c6855b162c
pierrehlvt <pierre.halavent@gmail.com>
2020-03-23 14:55:32 +0100

    contact form
    - import form-style.css in style.css
    - edit contact form

--- wp-content/themes/mynursery/template-contact.php
+++ wp-content/themes/mynursery/template-contact.php
@@ -8,36 +8,31 @@ get_header();
 
     <div class="container">
         <form action="" method="post" class="form-style">
-            <h2 class="text-center pt-4">Contactez nous</h2>
+            <h2 class="text-center pt-4">Contactez-nous</h2>
             <div class="form-row">
                 <div class="col-md-8 mx-auto mt-3">
                     <div class="form-group">
-                        <input type="text" class="form-control" name="name" id="name">
-                        <label for="name">Nom</label>
+                        <input type="text" class="form-control" name="name" id="name" placeholder="Votre nom">
                         <span class="input-highlight"></span>
                     </div>
                     <div class="form-group">
-                        <i class="glyphicon glyphicon-user"></i>
-                        <input type="email" class="form-control" name="email" id="email">
-                        <label for="email">Email</label>
+                        <input type="email" class="form-control" name="email" id="email" placeholder="john.doe@gmail.com">
                         <span class="input-highlight"></span>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" name="subject" id="subject">
-                        <label for="subject">Sujet</label>
+                        <input type="text" class="form-control" name="subject" id="subject" placeholder="Sujet de votre message">
                         <span class="input-highlight"></span>
                     </div>
                     <!--                    <div class="col-md-6">-->
                     <div class="form-group">
-                        <textarea name="message" id="message" class="form-control"></textarea>
-                        <label for="message">Message</label>
+                        <textarea name="message" id="message" class="form-control" placeholder="Votre message"></textarea>
                         <span class="input-highlight"></span>
                     </div>
                     <!--                    </div>-->
                 </div>
 
             </div>
-            <div class="col-md-5 mx-auto mt-5">
+            <div class="col-md-5 mx-auto mt-5 mb-5">
                 <input type="submit" class="btn btn-lg btn-danger btn-block">
             </div>
         </form>
