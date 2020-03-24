fee347ebc62351a9e4988131262a45790ee2aff5
pierrehlvt <pierre.halavent@gmail.com>
2020-03-24 11:16:48 +0100

    latest new
    - Change panel name, require custom-latestnew.php in functions.php

--- wp-content/themes/mynursery/functions.php
+++ wp-content/themes/mynursery/functions.php
@@ -6,4 +6,4 @@ require get_template_directory() . '/inc/parameters.php';
 
 require get_template_directory() . '/inc/image.php';
 
-require get_template_directory() . '/inc/parameters.php';
+require get_template_directory() . '/inc/custom/custom-latestnew.php';
