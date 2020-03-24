fffc35b7bd42211f6854b24994ebe1d7b60c0789
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2020-03-25 00:35:13 +0100

    delete a print_r on the head of the page

--- wp-content/themes/mynursery/template-home.php
+++ wp-content/themes/mynursery/template-home.php
@@ -16,7 +16,6 @@ get_header();
             );
 
             $query = new WP_Query($args);
-            print_r($query);
             ?>
                 <div id="demo" class="carousel slide carousel-fade" data-ride="carousel">
                     <!-- Indicators -->
