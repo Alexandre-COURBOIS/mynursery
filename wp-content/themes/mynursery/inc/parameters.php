e9c521d159b0e7a235f362d7a50d43c5aaf8b0f6
Yann Poher <yan.poher@gmail.com>
2020-03-25 11:25:00 +0100

    page about creation and redirection on link

--- wp-content/themes/mynursery/inc/parameters.php
+++ wp-content/themes/mynursery/inc/parameters.php
@@ -5,8 +5,13 @@ global $web;
 $web = array(
     'pages' => array(
         'home' => array(
-            'id' => 6,
+            'id' => 5,
             'slug' => '/'
-        )
+        ),
+        'about' => array(
+            'id' => 19,
+            'slug' => 'about',
+        ),
     )
+
 );
