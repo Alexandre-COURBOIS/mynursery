82e556a68924a67b9d92eeaba930c62e8b870ddb
stephane <github@lecuyer.eu>
2020-03-28 16:41:44 +0100

    Modified ID in parameters.php

--- wp-content/themes/mynursery/inc/parameters.php
+++ wp-content/themes/mynursery/inc/parameters.php
@@ -5,16 +5,32 @@ global $web;
 $web = array(
     'pages' => array(
         'home' => array(
-            'id' => 6,
+            'id' => 5,
             'slug' => '/'
         ),
         'about' => array(
-            'id' => 19,
+            'id' => 150,
             'slug' => 'about',
         ),
         'inscription' => array(
             'id' => 7,
             'slug' => 'inscription'
+        ),
+        'ajouter-un-employe' => array(
+            'id' => 26,
+            'slug' => 'ajouter-un-employe'
+        ),
+        'connexion' => array(
+            'id' => 14,
+            'slug' => 'connexion'
+        ),
+        'contact' => array(
+            'id' => 22,
+            'slug' => 'contact'
+        ),
+        'profil' => array(
+            'id' => 18,
+            'slug' => 'profil'
         ),
     )
 );
