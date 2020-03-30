2ab04be9e01eb2a89913b468334a21c56d793cd8
pierrehlvt <pierre.halavent@gmail.com>
2020-03-30 14:27:02 +0200

    functions.php
    add link to Form and Validation class

--- wp-content/themes/mynursery/functions.php
+++ wp-content/themes/mynursery/functions.php
@@ -17,3 +17,6 @@ require get_template_directory() . '/inc/custom/custom-specialization.php';
 require get_template_directory() . '/inc/custom/custom-aboutus.php';
 
 require get_template_directory() . '/inc/custom/custom-partners.php';
+
+require get_template_directory() . '/inc/service/Validation.php';
+require get_template_directory() . '/inc/service/Form.php';
