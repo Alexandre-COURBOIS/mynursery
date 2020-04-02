31b24a494569b3fe9c847e410718a5fcc736a767
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2020-04-02 18:37:44 +0200

    [HOTFIX] map page, and adding some hred location in Nav

--- wp-content/themes/mynursery/header.php
+++ wp-content/themes/mynursery/header.php
@@ -41,23 +41,17 @@
 
             <div class="collapse navbar-collapse text-center" id="collapsibleNavbar">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Home
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="#"></a>
-                            <a class="dropdown-item" href="#"></a>
-                            <a class="dropdown-item" href="#"></a>
-                            <a class="dropdown-item" href="#"></a>
-                            <a class="dropdown-item" href="#"></a>
-                            <a class="dropdown-item" href="#"></a>
-                        </div>
+                    <li class="nav-item">
+                        <a class="nav-link" href="home">Home</a>
+                    </li>
+                    <?php if (!empty($_SESSION) & is_numeric($_SESSION['login']['id']) ) {?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="map">Map</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
+                        <a class="nav-link" href="profil">Profil</a>
                     </li>
+                    <?php } ?>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Blog</a>
                     </li>
--- wp-content/themes/mynursery/template-map.php
+++ wp-content/themes/mynursery/template-map.php
@@ -3,27 +3,20 @@
 Template Name: map
 */
 session_start();
+
 get_header();
+
 ?>
 
     <body class="body_mapbox">
 
-    <div class="flex-parent viewport-full relative scroll-hidden">
-        <div class="flex-child w-full w240-mm absolute static-mm left bottom">
-            <div class="flex-parent flex-parent--column viewport-third h-full-mm hmax-full bg-white scroll-auto">
-                <div class="px12 py12 bg-white">
-                    <div class="txt-bold txt-m">Geocoding response object</div>
-                    <div class="txt-s">View the raw JSON response from your query.</div>
-                </div>
-                <div class="flex-child flex-child--grow px12 py12 scroll-auto">
-                    <pre id="json-response"
-                         class="txt-xs px12">Search for a place. Your results will be displayed here!</pre>
-                </div>
-            </div>
-        </div>
-        <div id="box" class="box"></div>
+    <div class="separator_map"></div>
+
+    <div class="container_map">
         <div class="flex-child flex-child--grow bg-darken10 viewport-twothirds viewport-full-mm" id="map"></div>
     </div>
+
+    <div class="separator_map"></div>
 
     </body>
 
