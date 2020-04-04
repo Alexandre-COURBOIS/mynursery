f30302d7d6d4090688d5b6a708cfa13bea76dfa6
Yann Poher <yan.poher@gmail.com>
2020-04-04 18:36:10 +0200

    panel admin work in progress about users and update

--- wp-content/themes/mynursery/inc/admin/admin-users.php
+++ wp-content/themes/mynursery/inc/admin/admin-users.php
@@ -30,6 +30,9 @@ function users_menu_page()
     if(!empty($_GET['single']) && is_numeric($_GET['single'])) {
         $id = $_GET['single'];
         users_admin_single($id,$urlBase);
+    } elseif(!empty($_GET['edit']) && is_numeric($_GET['edit'])) {
+        $id = $_GET['edit'];
+        users_admin_edit($id, $urlBase);
     } else {
         users_admin_listing($urlBase);
     }
@@ -83,8 +86,9 @@ function users_admin_single($id,$urlBase)
 {
     global $wpdb;
     $user = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}creche WHERE id_creche = $id" , OBJECT );
-    $espace = " ";
     ?>
+
+
     <p>N° Identifiant: <?= $user->id_creche; ?></p>
     <p>Nom de la crèche : <?= $user->nom_creche; ?></p>
     <p>Nom du/de la Gérant(e) : <?= $user->nom_gerant; ?></p>
@@ -97,8 +101,73 @@ function users_admin_single($id,$urlBase)
     <p>Numéro de Sécurité Social : <?= $user->num_secusocial; ?></p>
     <p>Effectif Maximum d'Enfant : <?= $user->effectif_maxenfant; ?></p>
     <p>Date d'inscription : <?= $user->created_at; ?></p>
+    <ul>
+        <li><a class="button" href="<?= $urlBase; ?>&edit=<?= $user->id_creche ?>">Modifier</a></li>
+    </ul>
+
     <?php
 }
+
+function users_admin_edit($id,$urlBase)
+{
+    global $wpdb;
+    $user = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}creche WHERE id_creche = $id" , OBJECT );
+    ?>
+
+<table>
+    <tr>
+        <th><label for="id">N° Identifiant:</label></th>
+        <td><p><?= $user->id_creche; ?></p></td>
+    </tr>
+    <tr>
+        <th><label for="nom_creche">Nom de la crèche :</label></th>
+        <td><input type="text" value="<?= $user->nom_creche; ?>"></td>
+    </tr>
+    <tr>
+        <th><label for="nom_gerant">Nom du/de la Gérant(e) :</label></th>
+        <td><input type="text" value="<?= $user->nom_gerant; ?>"></td>
+    </tr>
+    <tr>
+        <th><label for="prenom_gerant">Prénom du/de la Gérant(e) :</label></th>
+        <td><input type="text" value="<?= $user->prenom_gerant; ?>"></td>
+    </tr>
+    <tr>
+        <th><label for="">Email :</label></th>
+        <td><input type="text" value="<?= $user->email; ?>"></td>
+    </tr>
+    <tr>
+        <th><label for="telephone_creche">N. Téléphone de la crèche :</label></th>
+        <td><input type="text" value="<?= $user->telephone_creche; ?>"></td>
+    </tr>
+    <tr>
+        <th><label for=""></label></th>
+        <td><input type="text" value=""></td>
+    </tr>
+    <tr>
+        <th><label for="num_siret">Numéro de SIRET :</label></th>
+        <td><input type="text" value="<?= $user->num_siret; ?>"></td>
+    </tr>
+    <tr>
+        <th><label for="num_agrement">Numéro d'agrément :</label></th>
+        <td><input type="text" value="<?= $user->num_agrement; ?>"></td>
+    </tr>
+    <tr>
+        <th><label for="num_secusocial">Numéro de Sécurité Social :</label></th>
+        <td><input type="text" value="<?= $user->num_secusocial; ?>"></td>
+    </tr>
+    <tr>
+        <th><label for="effectif_maxenfant">Effectif Maximum d'Enfant :</label></th>
+        <td><input type="text" value="<?= $user->effectif_maxenfant; ?>"></td>
+    </tr>
+    <tr>
+        <th><label for="created_at">Date d'inscription : </label></th>
+        <td><p><?= $user->created_at; ?></p></td>
+    </tr>
+</table>
+
+<?php
+}
+
 
 
 
