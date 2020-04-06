eaf8f21db8286a7e04eb673085f660c46c68507f
pierrehlvt <pierre.halavent@gmail.com>
2020-04-06 18:05:28 +0200

    Calendar
    - remove debug

--- wp-content/themes/mynursery/template-requestreservation.php
+++ wp-content/themes/mynursery/template-requestreservation.php
@@ -5,10 +5,8 @@ Template Name: RequestReservation
 
 global $wpdb;
 
-$reservations = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}reservation INNER JOIN {$wpdb->prefix}enfant ON {$wpdb->prefix}reservation.id_enfant = {$wpdb->prefix}enfant.id_enfant INNER JOIN {$wpdb->prefix}enfant ON {$wpdb->prefix}reservation.id_enfant = {$wpdb->prefix}enfant.id_enfant");
-echo '<pre>';
-print_r($reservations);
-echo '</pre>';
+$reservations = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}reservation INNER JOIN {$wpdb->prefix}enfant ON {$wpdb->prefix}reservation.id_enfant = {$wpdb->prefix}enfant.id_enfant");
+
 echo json_encode($reservations);
 
 
