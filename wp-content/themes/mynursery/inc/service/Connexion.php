57498d9185026943222ca4f173e951f3b5011c73
stephane <github@lecuyer.eu>
2020-04-06 09:06:27 +0200

    Changing the connection to retrieve the parent user's data

--- wp-content/themes/mynursery/inc/service/Connexion.php
+++ wp-content/themes/mynursery/inc/service/Connexion.php
@@ -32,6 +32,19 @@ class Connexion
 
         header('Location: '.$header);
     }
+
+    public function InitializeSessionParent($user,$header)
+    {
+        $_SESSION['login'] = array(
+            'id' => $user->idresponsable_legale,
+            'nom' => $user->nom,
+            'prenom' => $user->prenom,
+            'sexe' => $user->sexe,
+            'ip' => $_SERVER['REMOTE_ADDR'],
+        );
+
+        header('Location: '.$header);
+    }
     
 
 }
