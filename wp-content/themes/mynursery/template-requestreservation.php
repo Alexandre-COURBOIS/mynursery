e1a5c166da2f146170e137574de2334f3f2eb1ae
pierrehlvt <pierre.halavent@gmail.com>
2020-04-07 11:03:10 +0200

    RequestCalendar
    - checking session state for the request

--- wp-content/themes/mynursery/template-requestreservation.php
+++ wp-content/themes/mynursery/template-requestreservation.php
@@ -2,11 +2,16 @@
 /*
 Template Name: RequestReservation
 */
+session_start();
 
 global $wpdb;
 
-$reservations = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}reservation INNER JOIN {$wpdb->prefix}enfant ON {$wpdb->prefix}reservation.id_enfant = {$wpdb->prefix}enfant.id_enfant");
+if(!empty($_SESSION) && !empty($_SESSION['login']['nom_creche'])) {
+    $reservations = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}reservation INNER JOIN {$wpdb->prefix}enfant ON {$wpdb->prefix}reservation.id_enfant = {$wpdb->prefix}enfant.id_enfant");
 
-echo json_encode($reservations);
+    echo json_encode($reservations);
+} else {
+    die('404');
+}
 
 
