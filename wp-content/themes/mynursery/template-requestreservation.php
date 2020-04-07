101f2b3a19e19f05cc45d3b2515935788da18385
pierrehlvt <pierre.halavent@gmail.com>
2020-04-07 17:01:46 +0200

    RequestCalendar
    - getting session id to verify it and display the nurse's reservation in terms of the id

--- wp-content/themes/mynursery/template-requestreservation.php
+++ wp-content/themes/mynursery/template-requestreservation.php
@@ -5,14 +5,14 @@ Template Name: RequestReservation
 session_start();
 
 global $wpdb;
-//print_r($_SESSION);
-//if(empty($_SESSION)) {
-   // $id = $_SESSION['login']['id'];
-    $reservations = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}reservation INNER JOIN {$wpdb->prefix}enfant ON {$wpdb->prefix}reservation.id_enfant = {$wpdb->prefix}enfant.id_enfant");
+
+if(!empty($_SESSION) && !empty($_SESSION['login']['nom_creche'])) {
+    $id = $_SESSION['login']['id'];
+    $reservations = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}reservation INNER JOIN {$wpdb->prefix}enfant ON {$wpdb->prefix}reservation.id_enfant = {$wpdb->prefix}enfant.id_enfant WHERE $id = {$wpdb->prefix}reservation.id_creche");
 
     echo json_encode($reservations);
-//} else {
-//    //die('404');
-//}
+} else {
+    die('404');
+}
 
 
