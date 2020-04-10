bb002ab55cbe0098e74f9b6df5ad69604e077a56
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2020-04-10 12:14:13 +0200

    navbar is now redirecting on the right part of the website

--- wp-content/themes/mynursery/inc/service/Connexion.php
+++ wp-content/themes/mynursery/inc/service/Connexion.php
@@ -10,6 +10,7 @@ class Connexion
     public function InitializeSession($user,$header)
     {
         $_SESSION['login'] = array(
+            'user' => 'pro',
             'id' => $user->id_creche,
             'nom_creche' => $user->nom_creche,
             'prenom' => $user->prenom_gerant,
@@ -23,6 +24,7 @@ class Connexion
     public function InitializeSessionParent($user,$header)
     {
         $_SESSION['login'] = array(
+            'user' => 'particulier',
             'id' => $user->idresponsable_legale,
             'nom' => $user->nom,
             'prenom' => $user->prenom,
