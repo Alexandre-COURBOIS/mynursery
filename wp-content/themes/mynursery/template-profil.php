0276de649c0a2b123453224d25dd7cdb63c1a528
Yann Poher <yan.poher@gmail.com>
2020-04-19 18:19:43 +0200

    css on creche wp profil page, responsive

--- wp-content/themes/mynursery/template-profil.php
+++ wp-content/themes/mynursery/template-profil.php
@@ -31,47 +31,62 @@ if (!empty($_SESSION)) {
 
             <div class="row">
                 <div class="col-sm">
-                    <h2 class="text-center title_profil"><?= $creches[0]->nom_creche; ?></h2>
+                    <h2  class="text-center title_profil"><?= $creches[0]->nom_creche; ?></h2>
+                </div>
+                <div class="col-sm">
+                    <img class="img-fluid img_profil" src="<?= get_template_directory_uri() ?>/asset/img/bandeau-profil.jpg"
+                         alt="image bandeau">
                 </div>
             </div>
 
             <div class="separator_map"></div>
 
             <div class="row">
-                <div class="col-6">
-                    <img class="img_profil" src="<?= get_template_directory_uri() ?>/asset/img/avatar-profil.png"
-                         alt="image de la personne">
+                <div class="col-md-6">
+
                     <div class="separator_map"></div>
-                    <p>Vos informations :</p>
-                    <div class="separator_map"></div>
-                    <p id="margin_profil">Nom : <?= $creches[0]->nom_gerant; ?></p>
-                    <p id="margin_profil">Prenom : <?= $creches[0]->prenom_gerant; ?></p>
-                    <p id="margin_profil">Adresse mail : <?= $creches[0]->email; ?></p>
-                    <p id="margin_profil">Numéro de téléphone : +33<?= $creches[0]->telephone_creche; ?></p>
+
+                    <h3 id="h3">Vos informations</h3>
+
+                    <p id="profil-label">Nom:</p>
+                    <p id="margin_profil"><?= $creches[0]->nom_gerant; ?></p>
+                    <p id="profil-label">Prenom:</p>
+                    <p id="margin_profil"><?= $creches[0]->prenom_gerant; ?></p>
+                    <p id="profil-label">Adresse mail:</p>
+                    <p id="margin_profil"><?= $creches[0]->email; ?></p>
+                    <p id="profil-label">Numéro de téléphone:</p>
+                    <p id="margin_profil">+33<?= $creches[0]->telephone_creche; ?></p>
+                    <p id="profil-label">Adresse:</p>
                     <?php if (!empty($creches[0]->supp_rue)) { ?>
-                        <p id="margin_profil">Adresse
-                            : <?= $creches[0]->num_rue . ' ' . $creches[0]->supp_rue . ' ' . $creches[0]->nom_rue; ?></p>
+                        <p id="margin_profil"><?= $creches[0]->num_rue . ' ' . $creches[0]->supp_rue . ' ' . $creches[0]->nom_rue . ' '. $creches[0]->codepostal .' '. $creches[0]->ville; ?></p>
                     <?php } else { ?>
-                        <p id="margin_profil">Adresse : <?= $creches[0]->num_rue . ' ' . $creches[0]->nom_rue; ?></p>
-                    <?php } ?>
-                    <p id="margin_profil">Code postal : <?= $creches[0]->codepostal; ?></p>
-                    <p id="margin_profil">Ville : <?= $creches[0]->ville; ?></p>
-                    <p id="margin_profil">Numéro de SIRET/SIREN : <?= $creches[0]->num_siret; ?></p>
-                    <p id="margin_profil">Numéro de sécurité social : <?= $creches[0]->num_secusocial; ?></p>
-                    <p id="margin_profil">Numéro d'agrement' : <?= $creches[0]->num_agrement; ?></p>
-                    <p id="margin_profil">Effectif d'enfant maximum : <?= $creches[0]->effectif_maxenfant; ?></p>
+                        <p id="margin_profil"><?= $creches[0]->num_rue . ' ' . $creches[0]->nom_rue  . ' '. $creches[0]->codepostal .', '. $creches[0]->ville; } ?></p>
+                    <p id="profil-label">Numéro de SIRET/SIREN:</p>
+                    <p id="margin_profil"><?= $creches[0]->num_siret; ?></p>
+                    <p id="profil-label">Numéro de sécurité social:</p>
+                    <p id="margin_profil"><?= $creches[0]->num_secusocial; ?></p>
+                    <p id="profil-label">Numéro d'agrement':</p>
+                    <p id="margin_profil"><?= $creches[0]->num_agrement; ?></p>
+                    <p id="profil-label">Effectif d'enfant maximum :</p>
+                    <p id="margin_profil"><?= $creches[0]->effectif_maxenfant; ?></p>
                     <a href="<?= add_query_arg('id', $creches[0]->id_creche, home_url() . '/edit'); ?>" type="button"
                        class="btn btn-primary btn-lg">Editer votre profil</a>
                 </div>
-                <div class="col-6">
+                <div class="col-md-6">
+
+                    <div class="separator_map"></div>
+
+                    <h3 id="h3">Vos employés</h3>
+
                     <table class="table">
                         <thead>
+                        <caption class="titre_tab"><a href="ajouter-un-employe" type="button" class="btn btn-primary btn-lg">Ajouter un employé</a></caption>
                         <tr>
-                            <th scope="col">Employés</th>
-                            <th scope="col">Nom</th>
-                            <th scope="col">Prenom</th>
-                            <th scope="col">Téléphone</th>
-                            <th scope="col">Suppression</th>
+                            <th id="profil-label" scope="col">Employés</th>
+                            <th id="profil-label" scope="col">Nom</th>
+                            <th id="profil-label" scope="col">Prenom</th>
+                            <th id="profil-label" scope="col">Téléphone</th>
+                            <th id="profil-label" scope="col">Suppression</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -90,7 +105,6 @@ if (!empty($_SESSION)) {
                         } ?>
                         </tbody>
                     </table>
-                    <a href="ajouter-un-employe" type="button" class="btn btn-primary btn-lg">Ajouter un employé</a>
                 </div>
             </div>
 
